1cb32277c98b4dc6d405fb584e0346a2f376d065
LeoMarius <mariusleo64@gmail.com>
2025-01-08 16:59:28 +0100

    Count char and worlds in book and publish report

--- main.py
+++ main.py
@@ -1,23 +1,44 @@
 def main():
     book_path = "books/frankenstein.txt"
-    book_text = get_book_text(book_path)
+    # book_text = get_book_text(book_path)
     # print(book_text)
-    num_words = count_words(book_text)
-    print(f"{num_words} words found in the document")
+    # num_words = count_words(book_text)
+    # print(f"{num_words} words found in the document")
 
-    letter_count = count_char(book_text)
-    print(letter_count)
+    # letter_count = count_char(book_text)
+    # print(letter_count)
+    print_report(book_path)
 
 def count_char(text):
     count ={}
 
     for letter in text:
         letter = letter.lower()
-        if letter in count :
-            count[letter]+=1
-        else :
-            count[letter]=1
-    return count
+        if letter.isalpha():
+            if letter in count :
+                count[letter]+=1
+            else :
+                count[letter]=1
+    return dict(sorted(count.items()))
+
+def print_report(book_path):
+
+    print(f"--- Begin report of {book_path} ---")
+
+    book_text = get_book_text(book_path)
+    num_words = count_words(book_text)
+
+    print(f"{num_words} words found in the document")
+
+    print("")
+
+    letters_count = count_char(book_text)
+
+    for letter in letters_count:
+        # print(letter)
+        print(f"The '{letter}' character was found '{letters_count[letter]}' times")
+    
+    print("--- End report ---")
 
 def get_book_text(book):
     file_contents = ""
